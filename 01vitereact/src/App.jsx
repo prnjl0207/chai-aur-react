19b8e1c1fb863594fab80d7a69e3ef4ed1463000
prnjl0207 <146756631+prnjl0207@users.noreply.github.com>
2025-08-18 11:07:42 +0000

    updated jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,12 @@
+import Chai from "./Chai";
 
 function App() {
-
   return (
     <>
-<h1>Hello vite react</h1>
+      <h1>Hello vite react | Pranjal Verma</h1>
+      <Chai />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,5 @@
+const Chai = () => {
+  return <h3 style={{ color: "red" }}>Chai is ready now!!!</h3>;
+};
+
+export default Chai;
